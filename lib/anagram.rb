ae971f86f9d9f790cdb58605898d9e864a772d3f
Chibukasia Nelson <chibukasianelson@gmail.com>
2022-08-18 00:07:32 +0300

    commit tree 8fe73ca07badd9320be9a0a4c07d49ace3b10a98

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match (words)
+        match_words = []
+        words.each do |each_word|
+            letters = each_word.chars
+            if letters.sort==@word.chars.sort
+                #puts @word.chars.sort
+                match_words << each_word
+            end
+        end
+        match_words
+    end
+end
+
